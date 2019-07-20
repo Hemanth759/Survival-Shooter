f526a7476af50e615ccbdf0d9ccb99ae124d2f2e
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-07-20 19:03:38 +0530

    added enemy moment and Navmesh AI to enemy

---
+++ Survival Shooter/Assets/MyProject/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+  // public variables
+  public Transform target;
+  public float smoothing = 5f;
+
+  Vector3 offset;
+
+  /// <summary>
+  /// Start is called on the frame when a script is enabled just before
+  /// any of the Update methods is called the first time.
+  /// </summary>
+  private void Start()
+  {
+    offset = this.transform.position - target.position;
+  }
+
+  /// <summary>
+  /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+  /// </summary>
+  private void FixedUpdate()
+  {
+    Vector3 targetPos = target.position + offset;
+    this.transform.position = Vector3.Lerp(this.transform.position, targetPos, smoothing * Time.deltaTime);
+  }
+}
