8ef27a808b240e22f73a929b00dbf1beaebd3388
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-07-20 16:43:59 +0530

    added player movement

--- Survival Shooter/Assets/MyProject/Scripts/PlayerMovement.cs
+++ Survival Shooter/Assets/MyProject/Scripts/PlayerMovement.cs
@@ -1,16 +1,69 @@
 ﻿using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class PlayerMovement : MonoBehaviour {
+  // public variables
+  public float speed = 6f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+  // private varialbles
+  private Vector3 movement;
+  private Animator animator;
+  private Rigidbody playerRigidbody;
+  private int floorMask;
+  private float camRayLength = 100f;
+  private Camera cam;
+
+  /// <summary>
+  /// Awake is called when the script instance is being loaded.
+  /// </summary>
+  void Awake() {
+    floorMask = LayerMask.GetMask ("Floor");
+    animator = this.GetComponent<Animator> ();
+    playerRigidbody = this.GetComponent<Rigidbody> ();
+    cam = Camera.main;
+  }
+
+  /// <summary>
+  /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+  /// </summary>
+  void FixedUpdate()
+  {
+    float h = Input.GetAxisRaw("Horizontal");
+    float v = Input.GetAxisRaw("Vertical");
+
+    // funtion to turn the player
+    turning();
+    // function to animate the player from idle to moving
+    animating(h, v);
+
+    if(h != 0f || v != 0f) {
+      // function to move the player object
+      move(h, v);
     }
+  }
+
+  void move(float horizontal, float vertical) {
+    movement.Set(horizontal, 0f, vertical);
+
+    movement = movement.normalized * speed * Time.deltaTime;
+
+    playerRigidbody.MovePosition(transform.position + movement);
+  }
+
+  void turning() {
+    Ray camRay = cam.ScreenPointToRay(Input.mousePosition);
+    RaycastHit floorHit;
+
+    if(Physics.Raycast(camRay, out floorHit, camRayLength, floorMask)) {
+      Vector3 mouseToPlayer = floorHit.point - transform.position;
+      mouseToPlayer.y = 0f;
+
+      Quaternion newRotation = Quaternion.LookRotation(mouseToPlayer);
+      playerRigidbody.MoveRotation(newRotation);
+    }
+  }
+
+  void animating(float horizontal, float vertical) {
+    bool walking = horizontal != 0f || vertical != 0f;
+    animator.SetBool("IsWalking", walking);
+  }
 }
