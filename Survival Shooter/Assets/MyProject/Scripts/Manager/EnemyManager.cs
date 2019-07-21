867beb5a4439f4550d9633d236ccca340335be53
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-07-21 18:47:21 +0530

    Added spawn points

--- Survival Shooter/Assets/MyProject/Scripts/Manager/EnemyManager.cs
+++ Survival Shooter/Assets/MyProject/Scripts/Manager/EnemyManager.cs
@@ -2,27 +2,50 @@
 
 public class EnemyManager : MonoBehaviour
 {
-    public PlayerHealth playerHealth;
-    public GameObject enemy;
-    public float spawnTime = 3f;
-    public Transform[] spawnPoints;
+  public PlayerHealth playerHealth;
+  public GameObject ZombunnyPrefab;
+  public GameObject ZomBearPrefab;
+  public GameObject HelephantPrefab;
+  public float spawnTimeOfBear = 3f;
+  public float spawnTimeOfBunny = 4f;
+  public float spawnTimeOfHelephant = 10f;
+  public Transform[] spawnPoints;
 
 
-    void Start ()
-    {
-        InvokeRepeating ("Spawn", spawnTime, spawnTime);
+  void Start () {
+    InvokeRepeating("SpawnBear", spawnTimeOfBear, spawnTimeOfBear);
+    InvokeRepeating("SpawnBunny", spawnTimeOfBunny, spawnTimeOfBunny);
+    InvokeRepeating("SpawnHelephant", spawnTimeOfHelephant, spawnTimeOfHelephant);
+  }
+
+
+  void SpawnBear () {
+    if(playerHealth.currentHealth <= 0f) {
+      return;
     }
 
+    int spawnPointIndex = Random.Range (0, spawnPoints.Length);
 
-    void Spawn ()
-    {
-        if(playerHealth.currentHealth <= 0f)
-        {
-            return;
-        }
+    Instantiate (ZomBearPrefab, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+  }
 
-        int spawnPointIndex = Random.Range (0, spawnPoints.Length);
-
-        Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+  void SpawnBunny () {
+    if(playerHealth.currentHealth <= 0f) {
+      return;
     }
+
+    int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+
+    Instantiate (ZombunnyPrefab, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+  }
+
+  void SpawnHelephant () {
+    if(playerHealth.currentHealth <= 0f) {
+      return;
+    }
+
+    int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+
+    Instantiate (HelephantPrefab, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+  }
 }
